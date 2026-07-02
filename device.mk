00d0bb66c64f9be9623dd69cf20952f5967bdcaf
kailua <kailua@project2by2.jp>
2026-07-02 20:58:50 +0900

    md07p: Switch to prebuilt composer

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set_bool,libgui,support_mtk_ged_kpi,true)
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service \
     android.hardware.memtrack-service.mediatek-mali \
     android.software.vulkan.deqp.level-2021-03-01.prebuilt.xml \
     android.software.opengles.deqp.level-2021-03-01.prebuilt.xml
